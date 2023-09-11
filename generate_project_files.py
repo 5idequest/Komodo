4af79cfee42c5e4e6354b2cc950ac36c09f3d2f6
Jack <riskybidniss@gmail.com>
2023-09-11 21:05:58 +0100

    Fix typo in generate_project_files.py

--- generate_project_files.py
+++ generate_project_files.py
@@ -3,7 +3,7 @@ import subprocess
 
 if platform.system() == 'Darwin':
   subprocess.run('cmake -B build/macOS -G Xcode', shell=True)
-if platform.system() == 'Windows':
+elif platform.system() == 'Windows':
   subprocess.run('cmake -B build/Windows')
 else:
   print('Operating system not supported!')
