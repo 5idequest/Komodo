09a65523ca103fc6b4564a2d08fcdcc363bd907c
Jack <riskybidniss@gmail.com>
2023-09-13 14:49:23 +0100

    Remove instance from public API

--- src/komodo/komodo.hpp
+++ src/komodo/komodo.hpp
@@ -4,7 +4,6 @@
 
 #ifdef KOMODO_BACKEND_OPENGL
 
-  #include "komodo/backend/opengl/instance/opengl_instance.hpp"
   #include "komodo/backend/opengl/window/opengl_window.hpp"
 
 #endif // KOMODO_BACKEND_OPENGL
