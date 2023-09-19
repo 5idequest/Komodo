d9269dd30948a8652765953bad86549b63c97af4
Jack <riskybidniss@gmail.com>
2023-09-19 16:34:59 +0100

    Vulkan window surfaces

--- src/komodo/backend/vulkan/window/vulkan_window.cpp
+++ src/komodo/backend/vulkan/window/vulkan_window.cpp
@@ -6,10 +6,16 @@
 
 namespace Komodo {
 
-VulkanWindow::VulkanWindow(const WindowCreateInfo& create_info) {
+VulkanWindow::VulkanWindow(VulkanInstanceData& instance, const WindowCreateInfo& create_info) : instance(instance) {
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
   CreateGlfwWindow(create_info, Vulkan::GenerateWindowTitle(create_info.title));
+
+  glfwCreateWindowSurface(instance.instance, glfw_window, instance.allocator, &window.surface);
+}
+
+VulkanWindow::~VulkanWindow() {
+  vkDestroySurfaceKHR(instance.instance, window.surface, instance.allocator);
 }
 
 }
--- src/komodo/backend/vulkan/window/vulkan_window.hpp
+++ src/komodo/backend/vulkan/window/vulkan_window.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
 #include "komodo/backend/glfw/window/glfw_window.hpp"
+#include "komodo/backend/vulkan/instance/vulkan_instance_data.hpp"
+#include "komodo/backend/vulkan/window/vulkan_window_data.hpp"
 
 namespace Komodo {
 
 class VulkanWindow : public GlfwWindow {
 public:
-  VulkanWindow(const WindowCreateInfo& create_info = { });
+  VulkanWindow(VulkanInstanceData& instance, const WindowCreateInfo& create_info);
+  ~VulkanWindow();
+
+private:
+  VulkanWindowData window;
+  VulkanInstanceData& instance;
 };
 
 }
