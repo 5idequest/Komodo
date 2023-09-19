3f0de4f7c8b5cfb3bcc5f8adc317201dad334d4e
Jack <riskybidniss@gmail.com>
2023-09-19 16:18:40 +0100

    Fix assert macro for release builds

--- src/komodo/debug/assert.hpp
+++ src/komodo/debug/assert.hpp
@@ -10,20 +10,19 @@
     } \
   } while(0)
 
-  #define KM_ASSERT_WITHOUT_MSG(x, what) KM_ASSERT_WITH_MSG(x, "Assert failed!")
-
-  #define KM_ASSERT_MACRO_CHOOSER(x, A, B, FUNC) FUNC
-  
-
-  #define KM_ASSERT(...) KM_ASSERT_MACRO_CHOOSER(, ##__VA_ARGS__,\
-                                                KM_ASSERT_WITH_MSG(__VA_ARGS__), \
-                                                KM_ASSERT_WITHOUT_MSG(__VA_ARGS__) \
-                                                )
-
 #endif // KOMODO_BUILD_DEBUG
 
 #ifdef KOMODO_BUILD_RELEASE
 
-  #define KM_ASSERT(x)
+  #define KM_ASSERT_WITH_MSG(x, what) x
 
 #endif
+
+#define KM_ASSERT_WITHOUT_MSG(x, what) KM_ASSERT_WITH_MSG(x, "Assert failed!")
+
+#define KM_ASSERT_MACRO_CHOOSER(x, A, B, FUNC) FUNC  
+
+#define KM_ASSERT(...) KM_ASSERT_MACRO_CHOOSER(, ##__VA_ARGS__,\
+                                              KM_ASSERT_WITH_MSG(__VA_ARGS__), \
+                                              KM_ASSERT_WITHOUT_MSG(__VA_ARGS__) \
+                                              )
