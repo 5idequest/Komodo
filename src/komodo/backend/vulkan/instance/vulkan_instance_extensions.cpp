dbb60eed6d432be94de6a30581afbeb8f6d74a2d
Jack <riskybidniss@gmail.com>
2023-09-19 12:52:06 +0100

    Add debug utils extension

--- src/komodo/backend/vulkan/instance/vulkan_instance_extensions.cpp
+++ src/komodo/backend/vulkan/instance/vulkan_instance_extensions.cpp
@@ -1,18 +1,30 @@
 #include "vulkan_instance_extensions.hpp"
 
 #include <cstdint>
+#include <vector>
 
 #include <GLFW/glfw3.h>
 
 namespace Komodo {
 namespace Vulkan {
 
-void EnableInstanceExtensions(VkInstanceCreateInfo& instance_info) {    
-  uint32_t glfw_extension_count = 0;
-  const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
-  instance_info.enabledExtensionCount = glfw_extension_count;
-  instance_info.ppEnabledExtensionNames = glfw_extensions;
-  instance_info.enabledLayerCount = 0;
+static std::vector<const char*> extensions;
+
+void EnableInstanceExtensions(VkInstanceCreateInfo& instance_info) {
+  if (extensions.empty()) {
+    uint32_t glfw_extension_count = 0;
+    const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+    for (int i = 0; i < glfw_extension_count; ++i) {
+      extensions.push_back(glfw_extensions[i]);
+    }
+
+#ifdef KOMODO_BUILD_DEBUG
+    extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+#endif
+  }
+
+  instance_info.enabledExtensionCount = extensions.size();
+  instance_info.ppEnabledExtensionNames = extensions.data();
 }
 
 }
