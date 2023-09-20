8f23fc0a22f99e85e04fb4f8e64d260c8ab97283
Jack <riskybidniss@gmail.com>
2023-09-20 16:35:52 +0100

    Add example shader API code to main.cpp

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -3,6 +3,11 @@
 int main() {
   Komodo::VulkanInstance komodo;
   auto window = komodo.CreateWindow();
+
+  Komodo::ShaderCreateInfo shader_info;
+  shader_info.vert_path = "res/shaders/test.vert.glsl";
+  shader_info.frag_path = "res/shaders/test.frag.glsl";
+  auto shader = komodo.CreateShader(shader_info);
   
   while (window->IsOpen()) {
     Komodo::PollWindowEvents();
