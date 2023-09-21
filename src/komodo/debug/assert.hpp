1ca3ec2d68aeb1f497407f990b50829740132d8e
Jack <riskybidniss@gmail.com>
2023-09-21 03:18:57 +0100

    Remove macro nonsense from assert

--- src/komodo/debug/assert.hpp
+++ src/komodo/debug/assert.hpp
@@ -4,7 +4,7 @@
 
   #include "komodo/debug/exception.hpp"
 
-  #define KM_ASSERT_WITH_MSG(x, what) do { \
+  #define KM_ASSERT(x, what) do { \
     if (!(x)) { \
       throw Exception(what); \
     } \
@@ -14,15 +14,6 @@
 
 #ifdef KOMODO_BUILD_RELEASE
 
-  #define KM_ASSERT_WITH_MSG(x, what) x
+  #define KM_ASSERT(x, what) x
 
 #endif
-
-#define KM_ASSERT_WITHOUT_MSG(x, what) KM_ASSERT_WITH_MSG(x, "Assert failed!")
-
-#define GET_3RD_ARG(arg1, arg2, arg3, arg4, ...) arg4
-#define KM_ASSERT_MACRO_CHOOSER(...) \
-    GET_3RD_ARG(__VA_ARGS__, \
-    KM_ASSERT_WITH_MSG, KM_ASSERT_WITHOUT_MSG, )
-
-#define KM_ASSERT(...) KM_ASSERT_MACRO_CHOOSER(__VA_ARGS__)(__VA_ARGS__)
